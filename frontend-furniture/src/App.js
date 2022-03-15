80cf9070b3ae0de4e3694b492dcc182183009bb9
Heather Mielke <heatherlmielke@gmail.com>
2022-03-15 17:22:27 -0500

    Added responsive navbar and started routes and added page files

--- frontend-furniture/src/App.js
+++ frontend-furniture/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/products" />
+        <Route path="/cart" />
+        <Route path="/admin" />
+        <Route path="/products/show" />
+      </Routes>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+
+export default App
